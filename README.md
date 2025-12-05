b7f75feffe767c4bce647383dc4c6676e87ec445
DollyShe <DollySh233@gmail.com>
2025-12-05 08:54:20 +0200

    [526338897] feat: add init state server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cyber-Security-Project
+
+# the GROUP_SEED is the following:
+GROUP_SEED = 526338897
---
+++ server.py
@@ -0,0 +1,12 @@
+class Server:
+    def __init__(self):
+        DB = {}
+    
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def login_totp(self):
+        pass
